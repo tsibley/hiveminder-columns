fb95999e1f8209b723850c0dbb9b77a42a971aa7
Thomas Sibley <tom@zulutango.org>
2011-09-20 19:27:53 -0400

    Add Group, Tags, and Locator columns

--- chrome/content/messenger.js
+++ chrome/content/messenger.js
@@ -1,20 +1,22 @@
-var dueHandler = {
-    getCellText: function(row, col) {
-        //get the message's header so that we can extract the reply to field
-        var key = gDBView.getKeyAt(row);
-        var hdr = gDBView.db.GetMsgHdrForKey(key);
-      
-        return hdr.getStringProperty("x-hiveminder-due");
-    },
-    getSortStringForRow: function(hdr) {
-        return hdr.getStringProperty("x-hiveminder-due");
-    },
-    isString:            function() { return true },
+function makeColumnHandler(header) {
+    return {
+        getCellText: function(row, col) {
+            //get the message's header so that we can extract the reply to field
+            var key = gDBView.getKeyAt(row);
+            var hdr = gDBView.db.GetMsgHdrForKey(key);
 
-    getCellProperties:   function(row, col, props){},
-    getRowProperties:    function(row, props){},
-    getImageSrc:         function(row, col) {return null;},
-    getSortLongForRow:   function(hdr) {return 0;}
+            return hdr.getStringProperty(header);
+        },
+        getSortStringForRow: function(hdr) {
+            return hdr.getStringProperty(header);
+        },
+        isString:            function() { return true },
+
+        getCellProperties:   function(row, col, props){},
+        getRowProperties:    function(row, props){},
+        getImageSrc:         function(row, col) {return null;},
+        getSortLongForRow:   function(hdr) {return 0;}
+    }
 }
 
 function onLoad() {
@@ -23,8 +25,11 @@ function onLoad() {
 
     var CreateDbObserver = {
         // Components.interfaces.nsIObserver
-        observe: function(aMsgFolder, aTopic, aData) {  
-            gDBView.addColumnHandler("colHiveminderDue", dueHandler);
+        observe: function(aMsgFolder, aTopic, aData) {
+            gDBView.addColumnHandler("colHiveminderDue", makeColumnHandler("x-hiveminder-due"));
+            gDBView.addColumnHandler("colHiveminderTags", makeColumnHandler("x-hiveminder-tags"));
+            gDBView.addColumnHandler("colHiveminderGroup", makeColumnHandler("x-hiveminder-group"));
+            gDBView.addColumnHandler("colHiveminderLocator", makeColumnHandler("x-hiveminder-recordlocator"));
         }
     };
 
