89187bb6f1ae6e1484404a14fe40672f7bbf51eb
Thomas Sibley <tom@zulutango.org>
2011-09-20 20:31:51 -0400

    Treat dates as integers rather than strings

--- chrome/content/messenger.js
+++ chrome/content/messenger.js
@@ -1,3 +1,30 @@
+function makeDateHandler(header) {
+    return {
+        getCellText: function(row, col) {
+            //get the message's header so that we can extract the reply to field
+            var key = gDBView.getKeyAt(row);
+            var hdr = gDBView.db.GetMsgHdrForKey(key);
+
+            return hdr.getStringProperty(header);
+        },
+        getSortLongForRow: function(hdr) {
+            var value = hdr.getStringProperty(header);
+            if (value) {
+                var date = new Date(value);
+                return date.getTime();
+            } else {
+                return 0;
+            }
+        },
+        getSortStringForRow: function(hdr) { return null },
+        isString:            function() { return false },
+
+        getCellProperties:   function(row, col, props){},
+        getRowProperties:    function(row, props){},
+        getImageSrc:         function(row, col) {return null;}
+    }
+}
+
 function makeColumnHandler(header) {
     return {
         getCellText: function(row, col) {
@@ -8,7 +35,7 @@ function makeColumnHandler(header) {
             return hdr.getStringProperty(header);
         },
         getSortStringForRow: function(hdr) {
-            return hdr.getStringProperty(header);
+            return hdr.getStringProperty(header) || '';
         },
         isString:            function() { return true },
 
@@ -26,7 +53,7 @@ function onLoad() {
     var CreateDbObserver = {
         // Components.interfaces.nsIObserver
         observe: function(aMsgFolder, aTopic, aData) {
-            gDBView.addColumnHandler("colHiveminderDue", makeColumnHandler("x-hiveminder-due"));
+            gDBView.addColumnHandler("colHiveminderDue", makeDateHandler("x-hiveminder-due"));
             gDBView.addColumnHandler("colHiveminderTags", makeColumnHandler("x-hiveminder-tags"));
             gDBView.addColumnHandler("colHiveminderGroup", makeColumnHandler("x-hiveminder-group"));
             gDBView.addColumnHandler("colHiveminderLocator", makeColumnHandler("x-hiveminder-recordlocator"));
